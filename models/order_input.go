640c7d9a1a9924c418a17d35085d22257d8b8b9c
Tobi Adeniji <adenijitobi94@gmail.com>
2024-12-24 19:15:59 +0100

    fixed incorrect struct tags syntax

--- models/order_input.go
+++ models/order_input.go
@@ -2,16 +2,16 @@ package models
 
 // OrderItemInput represents an individual item in the order payload.
 type OrderItemInput struct {
-    ProductID string `json:"product_id binding:"required"` // Product ID
-    Quantity  int    `json:"quantity binding:"required,min=1"`   // Quantity
+    ProductID string `json:"product_id" binding:"required"`
+    Quantity  int    `json:"quantity" binding:"required,min=1"`
 }
 
 // PlaceOrderInput represents the payload for placing an order.
 type PlaceOrderInput struct {
-    Items []OrderItemInput `json:"items"` // List of order items
+    Items []OrderItemInput `json:"items" binding:"required,dive"`
 }
 
 // UpdateOrderStatusInput represents the payload for updating the order status.
 type UpdateOrderStatusInput struct {
-    Status string `json:"status" json:"items" binding:"required,dive` // order status
+    Status string `json:"status" binding:"required"`
 }
